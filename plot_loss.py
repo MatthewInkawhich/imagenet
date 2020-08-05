571ec2675432707fb09cd6ad8ad54f19cca19f4d
MatthewInkawhich <matthewinkawhich@gmail.com>
2020-08-05 13:47:06 -0400

    plot_loss script plots with 3 axes

--- plot_loss.py
+++ plot_loss.py
@@ -7,13 +7,19 @@ Designed to take a log.txt file (logger output) as input.
 
 import argparse
 import os
+from mpl_toolkits.axes_grid1 import host_subplot
+import mpl_toolkits.axisartist as AA
 import matplotlib.pyplot as plt
 
 
 ##################################
 ### HELPERS
 ##################################
-
+def make_patch_spines_invisible(ax):
+    ax.set_frame_on(True)
+    ax.patch.set_visible(False)
+    for sp in ax.spines.values():
+        sp.set_visible(False)
 
 
 
@@ -27,16 +33,16 @@ def main():
         "logpath",
         type=str,
     )
-    parser.add_argument(
-        "--title",
-        type=str,
-        default="Loss Plot",
-    )
+    # parser.add_argument(
+    #     "--title",
+    #     type=str,
+    #     default="Loss Plot",
+    # )
     args = parser.parse_args()
 
     # Read file into list of lines
     lines = [line.rstrip('\n') for line in open(args.logpath)]
-    
+
     # Filter down to lines of interest
     good_lines = []
     for i in range(len(lines)):
@@ -96,11 +102,56 @@ def main():
 
 
     # Plot losses/accs
-    plt.plot(s1_training_epochs, s1_training_losses)
-    #plt.title("Training: {}".format(args.logpath))
-    plt.title("Training Loss: {}".format(args.title))
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
+    fig, host = plt.subplots()
+    fig.subplots_adjust(right=0.75)
+
+    par1 = host.twinx()
+    par2 = host.twinx()
+
+    # Offset the right spine of par2.  The ticks and label have already been
+    # placed on the right by twinx above.
+    par2.spines["right"].set_position(("axes", 1.2))
+    # Having been created by twinx, par2 has its frame off, so the line of its
+    # detached spine is invisible.  First, activate the frame but make the patch
+    # and spines invisible.
+    make_patch_spines_invisible(par2)
+    # Second, show the right spine.
+    par2.spines["right"].set_visible(True)
+
+    host.set_xlabel('Epoch')
+    host.set_ylabel('Training Accuracy')
+    par1.set_ylabel('Loss (Cross-Entropy)')
+    par2.set_ylabel('Loss (Smooth-L1)')
+
+    acc_color = 'green'
+    # ce_loss_color = '#fa433e'
+    # l1_loss_color = '#A42004'
+    ce_loss_color = 'tab:red'
+    l1_loss_color = 'tab:purple'
+
+    p1, = host.plot(s1_training_epochs, s1_training_accs, color=acc_color, linestyle='-')
+    p2, = host.plot(s2_training_epochs_cont, s2_training_accs, color=acc_color, linestyle='--')
+    p3, = par1.plot(s1_training_epochs, s1_training_losses, color=ce_loss_color, linestyle='-')
+    p4, = par2.plot(s2_training_epochs_cont, s2_training_losses, color=l1_loss_color, linestyle='--')
+
+    host.set_xlim(0, 135)
+    host.set_ylim(0, 80)
+    #par1.set_ylim(0, 4)
+    #par2.set_ylim(0, 1)
+
+    host.yaxis.label.set_color(p1.get_color())
+    par1.yaxis.label.set_color(p3.get_color())
+    par2.yaxis.label.set_color(p4.get_color())
+
+    tkw = dict(size=4, width=1.5)
+    host.tick_params(axis='y', colors=p1.get_color(), **tkw)
+    par1.tick_params(axis='y', colors=p3.get_color(), **tkw)
+    par2.tick_params(axis='y', colors=p4.get_color(), **tkw)
+    host.tick_params(axis='x', **tkw)
+
+    fig.suptitle(args.logpath.split('/')[-1])
+
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
     plt.show()
 
 
